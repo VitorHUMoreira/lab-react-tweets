f18f9e71001ef2a47c0fe955007a90d89494c389
VitorHUMoreira <vitorhugo_187@hotmail.com>
2022-08-29 17:58:25 -0300

    done

---
+++ src/components/Actions.js
@@ -0,0 +1,12 @@
+function Actions() {
+  return (
+    <>
+      <i class="far fa-comment"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+      <i class="fas fa-share"></i>
+    </>
+  );
+}
+
+export default Actions;
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,5 @@
+function ProfileImage({ img }) {
+  return <img src={img} className="profile" alt="profile" />;
+}
+
+export default ProfileImage;
